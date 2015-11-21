2d9213ce9d2e949f75110fb92c1c85e53b5052e1
skom2308 <jvcd2308@gmail.com>
2015-11-21 13:31:42 +0100

    commit 4

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ shinyUI(fluidPage(
                                     step = 1, animate=
                                             animationOptions(interval=666, loop=TRUE)),
                         hr(),
-                        helpText("INSTRUCTIONS: Click on the play button above to see animation or move the slider manually to se Telephones by Region between 1956 and 1961"),
+                        helpText("INSTRUCTIONS: Click on the play button above to see barplot animation of Telephones per region or move the slider manually to see Telephones by Region between 1956 and 1961"),
                         helpText("Data from AT&T (1961) The World's Telephones."),
                         helpText("Prepared by José Carrasquero as part of the course project in 'Developing Data Products' in the 'Data Science Specialization' of the Johns Hopkins University")
                 ),
